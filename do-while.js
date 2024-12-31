16ca38dd795f693838d944faf84f4126d37de749
hellosiyam <hellosiyambd@gmail.com>
2024-12-31 20:59:33 +0600

    dowhile

---
+++ do-while.js
@@ -0,0 +1,18 @@
+// let n=0;
+// while (n<5) {
+//     console.log(n);
+//     n++
+// }
+// let n = 0;
+// do {
+//     console.log(n);
+//     n++
+    
+// } 
+// while (n<5);
+
+let i = 0;
+do {
+    console.log(i);
+    i++
+} while (i<20);
