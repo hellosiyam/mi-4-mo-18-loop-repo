ac9d1ca99cfa19ab4e0e489010266bcb0f4c78c3
hellosiyam <hellosiyambd@gmail.com>
2024-12-31 20:15:35 +0600

    break.js

---
+++ break.js
@@ -0,0 +1,17 @@
+// for(let i = 0; i<50; i++){
+//     console.log(i);
+//     if (i>=15) {
+//         break;
+//     }
+    
+// }
+
+let i=100;
+while (i>50) {
+    console.log(i);
+    
+    if (i<=60) {
+        break;
+    }
+    i--
+}
