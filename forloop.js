e900afa3f8fd86f406c737b73f72f50d1e4909b7
hellosiyam <hellosiyambd@gmail.com>
2024-12-31 18:07:48 +0600

    for loop

---
+++ forloop.js
@@ -0,0 +1,18 @@
+// for (let num = 0; num < 20; num++) {
+//     console.log(num);
+    
+// }
+
+// for (let num = 1; num < 10; num=num+2) {
+//     console.log(num);
+    
+    
+// }
+let sum =0;
+for (let num = 0; num<20; num=num+2 ){
+    console.log("Numner",num);
+    sum=num+sum
+    console.log('sum=',sum);
+    
+    
+}
