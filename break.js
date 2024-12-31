2caec952699f1bf13443b16bd017b47f79faf092
hellosiyam <hellosiyambd@gmail.com>
2024-12-31 20:52:42 +0600

    continue.js

--- break.js
+++ break.js
@@ -6,12 +6,35 @@
     
 // }
 
-let i=100;
-while (i>50) {
-    console.log(i);
+// let i=100;
+// while (i>50) {
+//     console.log(i);
     
-    if (i<=60) {
-        break;
+//     if (i<=60) {
+//         break;
+//     }
+//     i--
+// }
+
+// for(let i=0; i<50; i++){
+//     console.log(i);
+//     if (i>40) {
+//         break
+//     }
+    
+// }
+
+// for (let i =0; i<50; i++){
+//     console.log(i);
+//     if (i>20) {
+//         break
+//     }
+// }
+
+for (let i = 0; i<50; i++){
+    console.log(i);
+    if (i>20) {
+       break 
     }
-    i--
+    
 }
---
+++ continue.js
@@ -0,0 +1,41 @@
+// for(let i=1; i<=10; i++){
+//     if (i%2==!0) {
+//         continue
+//     }
+//     console.log(i);
+    
+// }
+
+// let i = 0;
+// while (i<50) {
+//     i++;
+//     if (i%2!==0) {
+//         continue
+//     }
+//     console.log(i);
+// }
+
+// for(let i = 0; i<20; i++){
+//     if (i%2===0) {
+//         continue
+//     }
+//     console.log(i);
+    
+// }
+let i = 1;
+while (i<50) {
+    i++
+    if (i%2==!0) {
+       continue 
+    }
+    console.log(i);
+    
+}
+
+for(let i = 0; i<50; i++){
+    if (i%2==0) {
+        continue
+    }
+    console.log(i);
+    
+}
